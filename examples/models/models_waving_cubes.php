d6081eb497294b04f0ec5b3ed266eeeef3f8c642
Oraoto <oraoto@outlook.com>
2020-05-01 22:24:53 +0800

    Add waving cube example, it's slow...)

---
+++ examples/models/models_waving_cubes.php
@@ -0,0 +1,92 @@
+<?php
+// Initialization
+//--------------------------------------------------------------------------------------
+$screenWidth = 800;
+$screenHeight = 450;
+
+InitWindow($screenWidth, $screenHeight, "raylib [models] example - waving cubes");
+
+// Initialize the camera
+$camera = Camera3D(Vector3(30, 20, 30), Vector3(0, 0, 0), Vector3(0, 1, 0), 70, RL_CAMERA_PERSPECTIVE);
+
+// Specify the amount of blocks in each direction
+$numBlocks = 15;
+$numBlocks_half = $numBlocks / 2;
+
+SetTargetFPS(60);
+//--------------------------------------------------------------------------------------
+
+$raywhite = RAYWHITE();
+
+// Main game loop
+while (!WindowShouldClose())    // Detect window close button or ESC key
+{
+    // Update
+    //----------------------------------------------------------------------------------
+    $time = GetTime();
+
+    // Calculate time scale for cube position and size
+    $scale = (2.0 + sin($time)) * 0.7;
+
+    // Move camera around the scene
+    $cameraTime = $time * 0.3;
+    $position = $camera->position;
+    $position->x = cos($cameraTime) * 40.0;
+    $position->z = sin($cameraTime) * 40.0;
+    $camera->position = $position;
+
+    //----------------------------------------------------------------------------------
+
+    // Draw
+    //----------------------------------------------------------------------------------
+    BeginDrawing();
+
+        ClearBackground($raywhite);
+
+        BeginMode3D($camera);
+
+            DrawGrid(10, 5);
+
+            for ($x = 0; $x < $numBlocks; $x++)
+            {
+                for ($y = 0; $y < $numBlocks; $y++)
+                {
+                    for ($z = 0; $z < $numBlocks; $z++)
+                    {
+                        // Scale of the blocks depends on x/y/z positions
+                        $blockScale = ($x + $y + $z)/30.0;
+
+                        // Scatter makes the waving effect by adding blockScale over time
+                        $scatter = sin($blockScale * 20.0 + $time * 4.0);
+
+                        // Calculate the cube position
+                        $cubePos = Vector3(
+                            ($x - $numBlocks_half) * ($scale*3.0) + $scatter,
+                            ($y - $numBlocks_half) * ($scale*2.0) + $scatter,
+                            ($z - $numBlocks_half) * ($scale*3.0) + $scatter
+                        );
+
+                        // Pick a color with a hue depending on cube position for the rainbow color effect
+                        $cubeColor = ColorFromHSV(Vector3(((($x + $y + $z)*18)%360), 0.75, 0.9));
+
+                        // Calculate cube size
+                        $cubeSize = (2.4 - $scale) * $blockScale;
+
+                        // And finally, draw the cube!
+                        DrawCube($cubePos, $cubeSize, $cubeSize, $cubeSize, $cubeColor);
+                    }
+                }
+            }
+
+        EndMode3D();
+
+        DrawFPS(10, 10);
+
+    EndDrawing();
+    //----------------------------------------------------------------------------------
+}
+
+// De-Initialization
+//--------------------------------------------------------------------------------------
+CloseWindow();        // Close window and OpenGL context
+//--------------------------------------------------------------------------------------
